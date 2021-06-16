7003c1aac8b333e4ec63858f481cf121e9b3279a
Paulo Bruno <pbrunosousa@gmail.com>
2021-06-16 14:59:26 -0300

    Only plot images if using command line arg

--- irisFeatureExtraction.py
+++ irisFeatureExtraction.py
@@ -25,45 +25,52 @@ avg_kernel = np.ones((5,5), np.float32)/25
 
 avg = cv2.filter2D(src, -1, avg_kernel)
 med = cv2.medianBlur(src, 5)
-'''
-plt.subplot(131)
-plt.imshow(src)
-plt.xticks([])
-plt.yticks([])
 
-plt.subplot(132)
-plt.imshow(avg)
-plt.xticks([])
-plt.yticks([])
 
-plt.subplot(133)
-plt.imshow(med)
-plt.xticks([])
-plt.yticks([])
+if args.plot:
 
-plt.show()
-'''
+    plt.subplot(131)
+    plt.imshow(src)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.subplot(132)
+    plt.imshow(avg)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.subplot(133)
+    plt.imshow(med)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.show()
+    
+
 _, bin_src = cv2.threshold(src, 50, 255, cv2.THRESH_BINARY_INV)
 _, bin_avg = cv2.threshold(avg, 50, 255, cv2.THRESH_BINARY_INV)
 _, bin_med = cv2.threshold(med, 50, 255, cv2.THRESH_BINARY_INV)
-'''
-plt.subplot(131)
-plt.imshow(bin_src)
-plt.xticks([])
-plt.yticks([])
 
-plt.subplot(132)
-plt.imshow(bin_avg)
-plt.xticks([])
-plt.yticks([])
 
-plt.subplot(133)
-plt.imshow(bin_med)
-plt.xticks([])
-plt.yticks([])
+if args.plot:
 
-plt.show()
-'''
+    plt.subplot(131)
+    plt.imshow(bin_src)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.subplot(132)
+    plt.imshow(bin_avg)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.subplot(133)
+    plt.imshow(bin_med)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.show()
+
 
 #print(bin_avg)
 num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(bin_avg, 8, cv2.CV_32S)
@@ -107,8 +114,9 @@ cv2.circle(pupil_region, (pupil_cx, pupil_cy), int(pupil_radius), (255, 255, 255
 pupil = dst.copy()
 cv2.circle(pupil, (pupil_cx, pupil_cy), int(pupil_radius), (0, 0, 255), 2)
 
-plt.imshow(pupil)
-plt.show()
+if args.plot:
+    plt.imshow(pupil)
+    plt.show()
 
 cv2.imwrite('temp.jpg', dst)
 
@@ -150,17 +158,19 @@ cv2.circle(iris_region, (i[0],i[1]), i[2], (255,255,255), cv2.FILLED)
 iris_src = pupil.copy()
 cv2.circle(iris_src, (i[0],i[1]), i[2], (255,0,0), 2)
 
-cv2.imshow('detected circles', iris_src)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if args.plot:
+    cv2.imshow('detected circles', iris_src)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 iris = cv2.bitwise_and(iris_region, dst)
 iris = cv2.bitwise_and(iris, cv2.bitwise_not(pupil_region))
 
-cv2.imshow('iris', iris)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if args.plot:
+    cv2.imshow('iris', iris)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 x_0 = i[0] - i[2]
@@ -170,20 +180,23 @@ y_t = i[1] + i[2]
 
 so_iris = iris[y_0:y_t, x_0:x_t]
 
-cv2.imshow('so_iris', so_iris)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if args.plot:
+    cv2.imshow('so_iris', so_iris)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 img_cut = src[y_0:y_t, x_0:x_t]
 
-cv2.imshow('cut', img_cut)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if args.plot:
+    cv2.imshow('cut', img_cut)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 edges = cv2.Canny(img_cut,50,150,apertureSize = 3)
-cv2.imshow('edges', edges)
-cv2.waitKey(0)
+if args.plot:
+    cv2.imshow('edges', edges)
+    cv2.waitKey(0)
 
 lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=50, maxLineGap=10)
 if (lines):
@@ -191,9 +204,10 @@ if (lines):
         x1,y1,x2,y2 = line[0]
         cv2.line(img_cut,(x1,y1),(x2,y2),(0,255,0),2)
 
-cv2.imshow('lines', img_cut)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if args.plot:
+    cv2.imshow('lines', img_cut)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 def daugman_normalization(image, height, width, r_in, r_out):       # Daugman归一化，输入为640*480,输出为width*height
@@ -239,12 +253,16 @@ def daugman_normalization(image, height, width, r_in, r_out):       # Daugman归
     
 
 print(so_iris.shape)
-    
+
 normal = daugman_normalization(so_iris, 60, 360, pupil_radius, i[2])
-plt.imshow(normal)
-plt.show()
+
+if args.plot:
+    plt.imshow(normal)
+    plt.show()
 
 normal = cv2.cvtColor(normal, cv2.COLOR_BGR2GRAY)
 normal = cv2.equalizeHist(normal)
-plt.imshow(normal, cmap='gray')
-plt.show()
+
+if args.plot:
+    plt.imshow(normal, cmap='gray')
+    plt.show()
